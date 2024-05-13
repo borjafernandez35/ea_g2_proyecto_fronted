b5a8daac2bc49524824adcf4d1a3ae49630a91ac
Aran <aran.huarte@estudiantat.upc.edu>
2024-05-13 20:11:28 +0200

    Home screen integrada

--- lib/Resources/pallete.dart
+++ lib/Resources/pallete.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Pallete {
-  static const Color backgroundColor = Color.fromRGBO(33, 28, 32, 1);
-  static const Color greyColor = Color.fromRGBO(87, 90, 100, 1);
+  static const Color backgroundColor = Color.fromRGBO(0, 28, 42, 1);
+  static const Color greyColor = Color.fromRGBO(160, 160, 160, 1);
   static const Color whiteColor = Color.fromRGBO(152, 155, 165, 1);
   static const Color blueColor = Color.fromRGBO(31, 122, 140, 1);
   static const Color salmonColor = Color.fromRGBO(254, 95, 85, 1);
