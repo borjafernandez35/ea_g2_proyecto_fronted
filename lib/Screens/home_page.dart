61f4465f0ab9f1d2550f733ea1d4bcf3d1cf6175
Angela <angela.nunez@estudiantat.upc.edu>
2024-05-14 18:07:01 +0200

    home-login-register funcionando

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:spotfinder/Screens/chatScreen.dart';
-import 'package:spotfinder/Screens/home_users.dart';
 import 'package:spotfinder/Resources/pallete.dart';
-import 'package:spotfinder/Screens/register_screen.dart';
 import 'package:spotfinder/Widgets/paramTextBox.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
-
+import 'profile_screen.dart';
 
 class HomePage extends StatefulWidget {
   // ignore: prefer_const_constructors_in_immutables
@@ -21,151 +18,54 @@ class HomePage extends StatefulWidget {
 class _nameState extends State<HomePage> {
   static HomeController homeController = Get.put(HomeController());
   int _selectedIndex = 0;
-  
-  @override
-  Widget build(BuildContext context) {
-    const gradientStart = Colors.black;
-    const gradientEnd = Colors.transparent;
-
-    final _gradient = LinearGradient(
-      colors: [gradientStart, gradientEnd],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      stops: [0, 0.3]
-    );
-
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      drawer: Drawer(
-        backgroundColor: Pallete.greyColor,
 
   static final List<Widget> _widgetOptions = <Widget>[
-    Container(
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: Column(
-          children: [
-            DrawerHeader(
-              child: Image.asset(
-                'logo.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0),
-              child: Divider(
-                color:Pallete.backgroundColor,
-              ),
-            ),
-            Padding(
-              
-              //onPressed:NavigationDestination(icon: icon, label: label),
-              padding: EdgeInsets.only(left:25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.home,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  'Home',
-                  style: TextStyle(color: Colors.white),
-                  ),
-                onTap: () {
-                  Get.to(() => HomePage());
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.flood_outlined,
-                  color: Colors.white,
-
-                ),
-                title: Text(
-                'Activities',
-                style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  Get.to(() => UserListPage());
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.book,
-                  color: Colors.white,
-
-                ),
-                title: Text(
-                  'Chat',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  Get.to(() =>ChatScreen());
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.ad_units,
-                  color: Colors.white,
-
-                ),
-                title: Text(
-                'Register',
-                style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  Get.to(() =>RegisterScreen());
-                },
-              ),
-            ),
-          ],
-            const SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center, // Centers widgets horizontally
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ParamTextBox(controller: homeController.searchBarController, text: 'Scaperoom...'),
-                IconButton(
-                  icon: Icon(
+    Align(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centers widgets horizontally
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ParamTextBox(
+                  controller: homeController.searchBarController,
+                  text: 'Scaperoom...'),
+              IconButton(
+                icon: const Icon(
                     size: 40,
                     color: Pallete.backgroundColor,
-                    LineIcons.searchLocation
-                  ),
-                  onPressed: (){
-
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 20,),
-          ],
-        ),
+                    LineIcons.searchLocation),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
       ),
     ),
-    Text("Activities"),
-    Text("Chats"),
-    Text("Profile"),
+    const Text("Activities"),
+    const Text("Chats"),
+    ProfileScreen()
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     const gradientStart = Colors.black;
     const gradientEnd = Colors.transparent;
 
     final _gradient = LinearGradient(
-      colors: [gradientStart, gradientEnd],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      stops: [0, 0.3]
-    );
+        colors: [gradientStart, gradientEnd],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [0, 0.3]);
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -191,55 +91,34 @@ class _nameState extends State<HomePage> {
               gap: 8,
               activeColor: Pallete.salmonColor,
               iconSize: 24,
-              padding: EdgeInsets.symmetric(horizontal: 200, vertical: 12),
               tabBackgroundColor: Colors.grey[100]!,
               color: Pallete.whiteColor,
               tabs: const [
                 GButton(
-                iconColor: Colors.white,
-                icon: LineIcons.mapMarker,
-                text: "Home"
-                ),
+                    iconColor: Colors.white,
+                    icon: LineIcons.mapMarker,
+                    text: "Home"),
                 GButton(
-                iconColor: Colors.white,
-                icon: LineIcons.hiking,
-                text: "Activities"
-                ),
+                    iconColor: Colors.white,
+                    icon: LineIcons.hiking,
+                    text: "Activities"),
                 GButton(
-                iconColor: Colors.white,
-                icon: LineIcons.comment,
-                text: "Chats"
-                ),
+                    iconColor: Colors.white,
+                    icon: LineIcons.comment,
+                    text: "Chats"),
                 GButton(
-                iconColor: Colors.white,
-                icon: LineIcons.user,
-                text: "Profile"
-                ),
+                    iconColor: Colors.white,
+                    icon: LineIcons.user,
+                    text: "Profile"),
               ],
               selectedIndex: _selectedIndex,
-              onTabChange: (index){
+              onTabChange: (index) {
                 setState(() {
                   _selectedIndex = index;
                 });
               },
             ),
           ),
-        ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(gradient: _gradient),
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(
-                Icons.menu,
-                color: Pallete.salmonColor,
-              ),
-              onPressed: (){
-                Scaffold.of(context).openDrawer();
-              },
-            ),
-          ],
         ),
       ),
     );
--- lib/Screens/title_screen.dart
+++ lib/Screens/title_screen.dart
@@ -26,67 +26,58 @@ class TitleScreen extends StatelessWidget {
                 height: 200,
                 width: 200,
               ),
-              Positioned(
-                bottom: 200, // Ajusta la posición vertical aquí
-                left: 50,
-                child: Container(
-                  width: 200, // Ajusta el ancho del contenedor aquí
-                  height: 50, // Ajusta la altura del contenedor aquí
-                  margin: const EdgeInsets.all(
-                      20), // Establece el margen deseado aquí
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Acción al presionar el botón
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const LoginScreen()), // Navega a la pantalla de inicio de sesión
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Pallete
-                          .salmonColor, // Cambia el color del botón a salmón
-                    ),
-                    child: const Text(
-                      'Log in',
-                      style: TextStyle(
-                        fontSize: 26, // Ajusta el tamaño del texto aquí
-                        color: Colors.white, // Cambia el color del texto a blanco
-                      ),
+              Container(
+                margin: const EdgeInsets.all(
+                  20,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Acción al presionar el botón
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginScreen()), // Navega a la pantalla de inicio de sesión
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Pallete
+                        .salmonColor, // Cambia el color del botón a salmón
+                  ),
+                  child: const Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontSize: 26, // Ajusta el tamaño del texto aquí
+                      color: Colors.white, // Cambia el color del texto a blanco
                     ),
                   ),
                 ),
               ),
-              Positioned(
-                top: 100,
-                left: 50,
-                child: RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                    ),
-                    children: [
-                      const TextSpan(
-                        text: "Don't have an account? ",
-                      ),
-                      TextSpan(
-                        text: "Sign up",
-                        style: const TextStyle(
-                          color: Pallete
-                          .salmonColor,
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline, // Subraya el texto "Sign up"
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => RegisterScreen()),
-                            );
-                          },
-                      ),
-                    ],
+              RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
                   ),
+                  children: [
+                    const TextSpan(
+                      text: "Don't have an account? ",
+                    ),
+                    TextSpan(
+                      text: "Sign up",
+                      style: const TextStyle(
+                        color: Pallete
+                            .salmonColor,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline, // Subraya el texto "Sign up"
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                          );
+                        },
+                    ),
+                  ],
                 ),
               ),
             ],
