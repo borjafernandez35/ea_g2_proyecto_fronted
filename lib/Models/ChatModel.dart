a5814b1270d42f5016760bcb2863772307ce8885
borjafernandez35 <borjaf29@gmail.com>
2024-05-20 03:03:42 +0200

    authorization y user

--- lib/Models/ChatModel.dart
+++ lib/Models/ChatModel.dart
@@ -1,10 +1,12 @@
 class Message {
   String message;
   String sentByMe;
+  String userName;
+ 
 
-  Message({required this.message, required this.sentByMe});
+  Message({required this.message, required this.sentByMe, required this.userName });
 
   factory Message.fromJson(Map<String, dynamic> json) {
-    return Message(message: json["message"], sentByMe: json["sentByMe"]);
+    return Message(message: json["message"], sentByMe: json["sentByMe"], userName: json["userName"]);
   }
 }
