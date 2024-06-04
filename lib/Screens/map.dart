c6709c78d37a7fae4f721ef137ac2b5142e5cd12
Aran <aran.huarte@estudiantat.upc.edu>
2024-06-04 16:55:22 +0200

    Localizacion usuario

--- lib/Screens/map.dart
+++ lib/Screens/map.dart
@@ -1,19 +1,18 @@
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter/foundation.dart';
-import 'package:line_icons/line_icon.dart';
 import 'package:spotfinder/Resources/pallete.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:spotfinder/Models/ActivityModel.dart';
-import 'package:spotfinder/Screens/login_screen.dart';
-import 'package:spotfinder/Utils/phone_utils.dart';
-import 'package:spotfinder/Widgets/button_sign_up.dart';
 import 'package:spotfinder/Widgets/paramTextBox_sign_up.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:spotfinder/Services/ActivityService.dart';
 import 'package:get/get.dart';
+import 'package:spotfinder/Models/UserModel.dart';
+import 'package:spotfinder/Services/UserService.dart';
 
+late UserService userService;
 late ActivityService activityService;
 
 class MapScreen extends StatefulWidget {
@@ -29,6 +28,7 @@ class _MapScreen extends State<MapScreen> {
   late List<Activity> lista_activities;
   late List<Marker> markers = [];
   bool isLoading = true;
+  Position? position;
 
   @override
   void initState() {
@@ -39,81 +39,97 @@ class _MapScreen extends State<MapScreen> {
 
   void getData() async {
     try {
+      position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
       lista_activities = await activityService.getData();
+      markers.add(
+        Marker(
+          point: LatLng(position!.latitude, position!.longitude),
+          width: 60,
+          height: 60,
+          alignment: Alignment.centerLeft,
+          child: const Icon(
+            Icons.circle,
+            size: 20,
+            color: Pallete.salmonColor
+          ),
+        )
+      );
       for(var actividad in lista_activities){
         markers.add(
         Marker(
           point: LatLng(actividad.latitude, actividad.longitude),
-            width: 60,
-            height: 60,
-            alignment: Alignment.centerLeft,
-            child: GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      content: SingleChildScrollView(
-                        child: Card(
-                          color: Pallete.primaryColor,
-                          surfaceTintColor: Pallete.accentColor,
-                          elevation: 5,
-                          margin: EdgeInsets.all(10),
-                          child: Row(
-                            children: [
-                              // Left side: Image
-                              Container(
-                                width: 100,
-                                height: 100,
-                                child: Image.network(
-                                  'https://via.placeholder.com/100', // Replace with your image URL
-                                  fit: BoxFit.cover,
+          width: 60,
+          height: 60,
+          alignment: Alignment.centerLeft,
+          child: GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: SingleChildScrollView(
+                      child: Card(
+                        color: Pallete.primaryColor,
+                        surfaceTintColor: Pallete.accentColor,
+                        elevation: 5,
+                        margin: EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            // Left side: Image
+                            Container(
+                              width: 100,
+                              height: 100,
+                              child: Image.network(
+                                'https://via.placeholder.com/100', // Replace with your image URL
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            // Right side: Title, Description, and Value
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      actividad.name,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Description: ${actividad.description}',
+                                      style: const TextStyle(fontSize: 14),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'rate: ${actividad.rate.toString()} ⭐',
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                              // Right side: Title, Description, and Value
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        actividad.name,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        'Description: ${actividad.description}',
-                                        style: const TextStyle(fontSize: 14),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        'rate: ${actividad.rate.toString()} ⭐',
-                                        style: const TextStyle(
-                                          fontSize: 14,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                );
-              },
-              child: Icon(
-                Icons.location_pin,
-                size: 60,
-                color: Pallete.salmonColor
-              ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: const Icon(
+              Icons.location_pin,
+              size: 60,
+              color: Pallete.salmonColor
             ),
+          ),
         ),
       );
       }
@@ -135,19 +151,21 @@ class _MapScreen extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     if (isLoading) {
-      return Center(child: CircularProgressIndicator());
+      return const Center(child: CircularProgressIndicator());
     } else {
       return Stack(
         children: [
           FlutterMap(
-            options: const MapOptions(
-              initialCenter: LatLng(41.3874, 2.1686),
+            options: MapOptions(
+              initialCenter: LatLng(position!.latitude, position!.longitude),
               initialZoom: 12,
-              interactionOptions: InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom),
+              interactionOptions: const InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom),
             ),
             children: [
               openStreetMapTileLayer,
-              MarkerLayer(markers: markers),
+              MarkerLayer(
+                markers: markers
+              ),
             ],
           ),
           Align(
