040851a4a583c4e3b94673b0123869a24b6b44a9
borjafernandez35 <borjaf29@gmail.com>
2024-06-09 19:25:33 +0200

    login actualizado

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:spotfinder/Screens/home_page.dart';
 import 'package:spotfinder/Widgets/button_sign_in.dart';
 import 'package:spotfinder/Widgets/paramTextBox.dart';
 import 'package:spotfinder/Services/UserService.dart';
 import 'package:get/get.dart';
-import 'package:spotfinder/Resources/sign_in_button.dart';
-import 'package:spotfinder/Services/SignInService.dart';
 
 late UserService userService;
-late SignInService signInService;
+late String? id;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -22,125 +19,18 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreen extends State<LoginScreen> {
   final Controller controller = Get.put(Controller());
   bool _obscureText = true;
-  GoogleSignInAccount? _currentUser;
-  bool _isAuthorized = false;
-  String _contactText = '';
 
   @override
   void initState() {
     super.initState();
-     signInService = SignInService(clientId: '435863540335-3edtkmprvlpkb3j4ea522cvndn8mc7mr.apps.googleusercontent.com');
     userService = UserService();
-    signInService.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      setState(() {
-        _currentUser = account;
-        _isAuthorized = signInService.isAuthorized;
-      });
-      if (_currentUser != null && _isAuthorized) {
-        _handleGetContact(_currentUser!);
-      }
-    });
-
-    signInService.signInSilently();
+    id = Get.arguments?['id'];
   }
 
   void _togglePasswordVisibility() {
     setState(() {
       _obscureText = !_obscureText;
     });
-  }
- Future<void> _handleGetContact(GoogleSignInAccount user) async {
-    setState(() {
-      _contactText = 'Loading contact info...';
-    });
-
-    try {
-      await signInService.handleGetContact(user);
-      setState(() {
-        _contactText = signInService.contactText;
-      });
-    } catch (error) {
-      setState(() {
-        _contactText = 'Failed to load contacts.';
-      });
-    }
-  }
-
-  Future<void> _handleAuthorizeScopes() async {
-    try {
-      await signInService.handleAuthorizeScopes();
-      setState(() {
-        _isAuthorized = signInService.isAuthorized;
-      });
-      if (_currentUser != null && _isAuthorized) {
-        _handleGetContact(_currentUser!);
-      }
-    } catch (error) {
-      print('Error authorizing scopes: $error');
-    }
-  }
-
-  Future<void> _handleSignIn() async {
-    try {
-      await signInService.handleSignIn();
-    } catch (error) {
-      print('Error signing in: $error');
-    }
-  }
-
-  Future<void> _handleSignOut() async {
-    await signInService.handleSignOut();
-  }
-
- Widget _buildBody() {
-    final GoogleSignInAccount? user = _currentUser;
-    if (user != null) {
-      // The user is Authenticated
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(identity: user),
-            title: Text(user.displayName ?? ''),
-            subtitle: Text(user.email),
-          ),
-          const Text('Signed in successfully.'),
-          if (_isAuthorized) ...<Widget>[
-            // The user has Authorized all required scopes
-            Text(_contactText),
-            ElevatedButton(
-              child: const Text('REFRESH'),
-              onPressed: () => _handleGetContact(user),
-            ),
-          ],
-          if (!_isAuthorized) ...<Widget>[
-            // The user has NOT Authorized all required scopes.
-            const Text('Additional permissions needed to read your contacts.'),
-            ElevatedButton(
-              onPressed: _handleAuthorizeScopes,
-              child: const Text('REQUEST PERMISSIONS'),
-            ),
-          ],
-          ElevatedButton(
-            onPressed: _handleSignOut,
-            child: const Text('SIGN OUT'),
-          ),
-        ],
-      );
-    } else {
-      // The user is NOT Authenticated
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const Text('You are not currently signed in.'),
-          // This method is used to separate mobile from web code with conditional exports.
-          // See: src/sign_in_button.dart
-          buildSignInButton(
-            onPressed: _handleSignIn,
-          ),
-        ],
-      );
-    }
   }
 
   @override
@@ -224,15 +114,13 @@ class _LoginScreen extends State<LoginScreen> {
                         'Forgot password?',
                         style: TextStyle(
                           color: Colors.white,
-                             decoration: TextDecoration.underline,
+                          decoration: TextDecoration.underline,
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
-              const SizedBox(height: 20),
-              _buildBody(),
             ],
           ),
         ],
@@ -261,10 +149,15 @@ class Controller extends GetxController {
           email: mailController.text,
           password: contrasenaController.text,
         );
+
         userService.logIn(logIn).then((statusCode) {
           // La solicitud se completó exitosamente, puedes realizar acciones adicionales si es necesario
           print('Usuario logeado exitosamente');
-          Get.to(() => HomePage());
+          if (id != null) {
+            Get.toNamed('/activity/$id');
+          } else {
+            Get.toNamed('/home');
+          }
         }).catchError((error) {
           // Manejar errores de solicitud HTTP
           Get.snackbar(
